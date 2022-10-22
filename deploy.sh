579a59916691632f4a31574f5dd6b6e79431d4bb
Nicolas Trutmann <nicolas.trutmann@gmx.ch>
2022-10-22 22:25:25 +0200

    added install-software things

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,14 @@
 DOTDIR=`pwd`
 SCRDIR='${DOTDIR}/scripts'
 
+## setup bashrc
 echo ". ${SCRDIR}/dotf_bashrc" >> ~/.bashrc
 
+## setup i3wm
 cat ${SCRDIR}/dotf_i3wm >> ~/.i3/config
 
+## install software
+awk '{print $1}' 2b_installed | sudo xargs pacman -Syu --noconfirm
+
 ## TODO: tmux.conf deploy
+
