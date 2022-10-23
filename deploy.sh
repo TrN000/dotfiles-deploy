be0b4f212df265d12ac3131372c1dd0b78802d76
Nicolas Trutmann <nicolas.trutmann@gmx.ch>
2022-10-23 10:48:36 +0200

    fixed pathnames and added vimrc stuff

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
 #### deploy script for dot files
 
-DOTDIR=`pwd`
-SCRDIR='${DOTDIR}/scripts'
+DOTDIR=$(dirname -- $(realpath -- "$0"))
+SCRDIR="${DOTDIR}/scripts"
 
 ## setup bashrc
 echo ". ${SCRDIR}/dotf_bashrc" >> ~/.bashrc
 
 ## setup i3wm
 cat ${SCRDIR}/dotf_i3wm >> ~/.i3/config
+
+## setup vimrc
+ln -s ${SCRDIR}/.vimrc ~/.vimrc
 
 ## install software
 awk '{print $1}' 2b_installed | sudo xargs pacman -Syu --noconfirm
