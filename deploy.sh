7b884b11ebf0eada266ab5da5a81aaf7455ee1c2
Nicolas Trutmann <nicolas.trutmann@gmx.ch>
2022-10-29 23:00:35 +0200

    added tmux config

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,11 @@ SCRDIR="${DOTDIR}/scripts"
 echo ". ${SCRDIR}/dotf_bashrc" >> ~/.bashrc
 
 ## setup i3wm
-cat ${SCRDIR}/dotf_i3wm >> ~/.i3/config
+echo "include ${SCRDIR}/dotf_i3wm" >> ~/.i3/config
 
 ## setup vimrc
 ln -s ${SCRDIR}/.vimrc ~/.vimrc
-## TODO: install all plugins here
+## TODO: install all plugins here -> vundle
 
 ## install software
 awk '{print $1}' 2b_installed | sudo xargs pacman -Syu --noconfirm
@@ -19,5 +19,7 @@ awk '{print $1}' 2b_installed | sudo xargs pacman -Syu --noconfirm
 ## install from AUR
 awk '{print $1}' 2b_installed_AUR | sudo xargs yay -Syu --noconfirm
 
-## TODO: tmux.conf deploy
+## tmux.conf deploy
+mkdir -p ~/.config/tmux/tmux.conf
+ln -s ${SCRDIR}/tmux.conf ~/.config/tmux/tmux.conf
 
